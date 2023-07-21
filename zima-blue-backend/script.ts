469d5244959dcb2940fb70ee30c934217248f258
Hasan <hasaneke1000@gmail.com>
2023-07-22 02:15:09 +0300

    Initial commit

---
+++ zima-blue-backend/script.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
+  console.dir(usersWithPosts, { depth: null })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
---
+++ zimablue/android/app/src/main/kotlin/com/zimablue/zimablue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zimablue.zimablue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
